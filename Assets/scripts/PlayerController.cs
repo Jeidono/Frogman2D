9680a5dc39e33641a7d855303b546bd1846778e8
Jay <Byrnejay2001@gmail.com>
2025-02-23 22:47:31 +1100

    Update Player flip

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -11,24 +11,32 @@ public class PlayerController : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask groundLayer;
     private bool isTouchingGround;
+
+
+    private Animator playerAnimation;
     
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    
         isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         direction = Input.GetAxis("Horizontal");
 
         if(direction > 0f) {
             player.linearVelocity = new Vector2(direction * speed, player.linearVelocity.y);
+            transform.localScale = new Vector2(2f,2f);
         }
+    
         else if (direction < 0f) {
             player.linearVelocity = new Vector2(direction * speed, player.linearVelocity.y);
+            transform.localScale = new Vector2(-2f,2f);
         }
         else {
              player.linearVelocity = new Vector2(0, player.linearVelocity.y);
@@ -37,5 +45,8 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyDown("space") && isTouchingGround){
             player.linearVelocity = new Vector2(player.linearVelocity.x, jumpSpeed);
         }
+
+        playerAnimation.SetFloat("Speed", Mathf.Abs(player.linearVelocity.x));
+        playerAnimation.SetBool("OnGround", isTouchingGround);
     }
 }
