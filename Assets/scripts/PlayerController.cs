1a82ce2ead8c41bd2123239cfab585393e7a70dd
Jay <Byrnejay2001@gmail.com>
2025-02-23 21:57:16 +1100

    jump functionality 2.0

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
     private float direction = 0f;
     public float jumpSpeed =8f;
     private Rigidbody2D player;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
     
     void Start()
     {
@@ -15,6 +20,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         direction = Input.GetAxis("Horizontal");
 
         if(direction > 0f) {
@@ -27,7 +34,7 @@ public class PlayerController : MonoBehaviour
              player.linearVelocity = new Vector2(0, player.linearVelocity.y);
         }
 
-        if(Input.GetKeyDown("space")){
+        if(Input.GetKeyDown("space") && isTouchingGround){
             player.linearVelocity = new Vector2(player.linearVelocity.x, jumpSpeed);
         }
     }
